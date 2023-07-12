1e6f5ac9a09935e14b757dadb45c9fba41ebe3d9
Rishav Pratik <85007441+RishavPratik@users.noreply.github.com>
2023-07-12 20:04:08 +0530

    challenge-2.sh

--- meta-data.sh
+++ meta-data.sh
@@ -1,2 +1,4 @@
+We need to write code that will query the meta data of an instance within AWS or Azure or GCP and provide a json formatted output. The choice of language and implementation is up to you.
+
 az account set --subscription SubscriptionName
-Az vm get-instance-view --resource-group Resource-group-Name-001 -name "Virtual-Machine-Name-001" --output json
+az vm get-instance-view --resource-group Resource-group-Name-001 -name "Virtual-Machine-Name-001" --output json
